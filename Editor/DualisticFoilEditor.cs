ad859a6dbff8dd68f91355921383ba6ce739e52d
pumachen <18906012399@163.com>
2021-01-26 16:40:01 +0800

    Init

---
+++ Editor/DualisticFoilEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DualisticFoil))]
+public class DualisticFoilEditor : Editor
+{
+    private new DualisticFoil target => base.target as DualisticFoil;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Render"))
+        {
+            string path = EditorUtility.SaveFilePanel(
+                "Save As", 
+                Application.dataPath, 
+                "Flipbook.png", 
+                ".png");
+            target.Render(path);
+        }
+    }
+}
